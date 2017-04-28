9333554465633d0b115b540688f9f64b16a2e25c
Jeremy Barzas <jbarzas@gmail.com>
2017-04-28 01:16:46 -0500

    more things are happening as intended

--- Text_Based_Adventure/Assets/Scripts/GridGenerator.cs
+++ Text_Based_Adventure/Assets/Scripts/GridGenerator.cs
@@ -6,19 +6,17 @@ public class GridGenerator : MonoBehaviour
 {
     public Vector2 gridSize;
     public GameObject groundPlanePrefab;
-    public GameObject nodePrefab;    
+    public GameObject nodePrefab;
     public GameObject playerPrefab;
 
-    private GameObject tmpNode;
-
+    private GameObject actualPlayerObject;
     private GameObject playerStartNode;
     private List<GameObject> nodes;
-	
+
     // Use this for initialization
-	void Start()
+    void Start()
     {
-        // ground plane spawn
-        Instantiate<GameObject>(groundPlanePrefab, this.transform);
+        // initialize node list
         nodes = new List<GameObject>();
 
         // set up limtis of grid size
@@ -28,35 +26,96 @@ public class GridGenerator : MonoBehaviour
         float minX = ((gridSize.x - 1) / 2) * -22.5f;
         float minY = ((gridSize.y - 1) / 2) * -22.5f;
 
+        // ground plane spawn
+        Instantiate<GameObject>(groundPlanePrefab, this.transform);
+
+        // setting up local variables for position
+        Vector3 newPos = nodePrefab.transform.position;
+        var tmpx = nodePrefab.transform.position.x;
+        var tmpy = nodePrefab.transform.position.y;
+        var tmpz = nodePrefab.transform.position.z;
+
+         // spawn center node  2,2 
+        newPos = new Vector3(tmpx - maxX + maxX, tmpy, tmpz - maxY + maxY);
+        var newNode0 = Instantiate<GameObject>(nodePrefab, this.transform);
+        newNode0.transform.position = newPos;
+        nodes.Add(newNode0);
+
+        // spawn bottom left node  0,0 
+        newPos = new Vector3(tmpx - maxX, tmpy, tmpz - maxY);
+        var newNode1 = Instantiate<GameObject>(nodePrefab, this.transform);
+        newNode1.transform.position = newPos;
+        nodes.Add(newNode1);
+
+        // spawn bottom right node 4,0
+        newPos = new Vector3(tmpx - minX, tmpy, tmpz - maxY);
+        var newNode2 = Instantiate<GameObject>(nodePrefab, this.transform);
+        newNode2.transform.position = newPos;
+        nodes.Add(newNode2);
+
+        // spawn top left node  0,4 
+        newPos = new Vector3(tmpx - maxX, tmpy, tmpz - minY);
+        var newNode3 = Instantiate<GameObject>(nodePrefab, this.transform);
+        newNode3.transform.position = newPos;
+        nodes.Add(newNode3);
+
+        // spawn top right node  4,4 
+        newPos = new Vector3(tmpx - minX, tmpy, tmpz - minY);
+        var newNode4 = Instantiate<GameObject>(nodePrefab, this.transform);
+        newNode4.transform.position = newPos;
+        nodes.Add(newNode4);
+        
         // node spawn
         while (nodes.Count != nodeCount)
         {
-            var tmpx = nodePrefab.transform.position.x;
-            var tmpy = nodePrefab.transform.position.y;
-            var tmpz = nodePrefab.transform.position.z;
+            // spawn all nodes to center of grid
+            newPos = new Vector3(tmpx - maxX + maxX, tmpy, tmpz - maxY + maxY);
             var newNode = Instantiate<GameObject>(nodePrefab, this.transform);
-            Vector3 newPos = new Vector3(tmpx - 45f, tmpy, tmpz - 45f);      
             newNode.transform.position = newPos;
-            nodes.Add(newNode);
+            nodes.Add(newNode);          
         }
         
-        // randomized player spawn
-        int index = Random.Range(0, nodeCount - 1);
-        playerStartNode = nodes[index];
-
         // fixed player spawn
         playerStartNode = nodes[0];
         var nodex = playerStartNode.transform.position.x;
         var nodey = playerStartNode.transform.position.y;
         var nodez = playerStartNode.transform.position.z;
         var player = Instantiate<GameObject>(playerPrefab);
-        Vector3 newPos2 = new Vector3(nodex, nodey + 5f, nodez);
-        player.transform.position = newPos2;
+        Vector3 playerPos = new Vector3(nodex, nodey + 5f, nodez);
+        player.transform.position = playerPos;
+        actualPlayerObject = player;
     }
 	
 	// Update is called once per frame
 	void Update()
     {
-		
-	}
+        // randomized player spawn        
+        int index = Random.Range(0, nodes.Count);
+        playerStartNode = nodes[index];
+        var nodex = playerStartNode.transform.position.x;
+        var nodey = playerStartNode.transform.position.y;
+        var nodez = playerStartNode.transform.position.z;
+        Vector3 playerPos = new Vector3(nodex, nodey + 5f, nodez);
+        actualPlayerObject.transform.position = playerPos;
+        
+        //// mouse click the nodes to move the player to its location
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    foreach (GameObject node in nodes)
+        //    {
+        //        for(int i = 0; i > 10; i++)
+        //        {
+        //            if (Input.mousePosition == node.transform.position)
+        //            {
+        //                var nodex1 = node.transform.position.x;
+        //                var nodey1 = node.transform.position.y;
+        //                var nodez1 = node.transform.position.z;
+        //                Vector3 playerPos1 = new Vector3(nodex1, nodey1 + 5f, nodez1);
+        //                actualPlayerObject.transform.position = playerPos1;
+        //            }
+        //        }
+        //    }           
+        //}
+        //Debug.Log(Input.mousePosition);
+    }
 }
